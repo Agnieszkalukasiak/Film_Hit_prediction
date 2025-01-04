1003e9b2e29b502ae5ca23dcfa0bb40856897738
Agnieszka Lukasiak <lukasiak@me.com>
2025-01-04 14:42:53 +0000

    finished the film_success_study streamlit page and added the png of plots

--- app_pages/Film_success_study.py
+++ app_pages/Film_success_study.py
@@ -45,7 +45,7 @@ def film_success_study_body():
 
       # Display the plot of general film profitibility
         st.image(
-     "/workspace/Film_Hit_prediction/outputs/figures/Percentage_of_Profitable_Movies_plot.png",
+    "/workspace/Film_Hit_prediction/outputs/figures/Percentage_of_Profitable_Movies_plot.png",
         caption="Percentage of Profitable Movies",
         use_column_width=True
     )
@@ -54,12 +54,12 @@ def film_success_study_body():
 
     # Display the plot of film production by genre 
         st.image(
-     "/workspace/Film_Hit_prediction/outputs/figures/Number_of_movies_produced_by_genre.png",
+    "/workspace/Film_Hit_prediction/outputs/figures/Number_of_movies_produced_by_genre.png",
         caption="Percentage of Profitable Movies",
         use_column_width=True
     )
 
-      # Divider
+    # Divider
     st.write("---") 
 
         # Revenue
@@ -68,14 +68,14 @@ def film_success_study_body():
         f"* It's profitibility and what gengre is the most produced. \n "
     )
 
-      # Display the plot of general film profitibility
+    # Display the plot of general film profitibility
     st.image(
      "/workspace/Film_Hit_prediction/outputs/figures/Percentage_of_Profitable_Movies_plot.png",
         caption="Percentage of Profitable Movies",
         use_column_width=True
     )
 
-     # Divider
+    # Divider
     st.write("---") 
 
     # Correlation Study Summary
@@ -92,75 +92,16 @@ def film_success_study_body():
         f"* The higher the budget, the higher the revenue. \n"
         f"* The genres most corelating to high revenue is Adventure, Action, Animation, Comedy, Fantasy and SiFi. \n"
         f"* The language most connected to high revenue are: English, Japanese, Telugu (south Indian), Chinese. \n"
-        f"* . \n"
     )
+def show_analysis_plots():
+    if st.checkbox("Show Correltion Analysis"):
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.image("outputs/figures/revenue_correlations_heatmap.png", caption="Revenue Correlation ")
+            st.image("outputs/figures/Budget_vs_Revenue_scatter_points.png", caption="Budgets Correlation To Revenue")
+            
+        with col2:
+            st.image("outputs/figures/Average_Revenue_by_Genre_plot.png", caption=" Genre Correlation To Revenue")
+            st.image("outputs/figures/Average_Revenue_by_Language.png", caption="Language Correlation To Revenue")
 
-    # Code copied from "02 - Churned Customer Study" notebook - "EDA on selected variables" section
-    df_eda = df.filter(vars_to_study + ['Churn'])
-
-    # Individual plots per variable
-    if st.checkbox("Churn Levels per Variable"):
-        churn_level_per_variable(df_eda)
-
-    # Parallel plot
-    if st.checkbox("Parallel Plot"):
-        st.write(
-            f"* Information in yellow indicates the profile from a churned customer")
-        parallel_plot_churn(df_eda)
-
-
-# function created using "02 - Churned Customer Study" notebook code - "Variables Distribution by Churn" section
-def churn_level_per_variable(df_eda):
-    target_var = 'Churn'
-
-    for col in df_eda.drop([target_var], axis=1).columns.to_list():
-        if df_eda[col].dtype == 'object':
-            plot_categorical(df_eda, col, target_var)
-        else:
-            plot_numerical(df_eda, col, target_var)
-
-
-# code copied from "02 - Churned Customer Study" notebook - "Variables Distribution by Churn" section
-def plot_categorical(df, col, target_var):
-    fig, axes = plt.subplots(figsize=(12, 5))
-    sns.countplot(data=df, x=col, hue=target_var,
-                  order=df[col].value_counts().index)
-    plt.xticks(rotation=90)
-    plt.title(f"{col}", fontsize=20, y=1.05)
-    st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
-
-
-# code copied from "02 - Churned Customer Study" notebook - "Variables Distribution by Churn" section
-def plot_numerical(df, col, target_var):
-    fig, axes = plt.subplots(figsize=(8, 5))
-    sns.histplot(data=df, x=col, hue=target_var, kde=True, element="step")
-    plt.title(f"{col}", fontsize=20, y=1.05)
-    st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
-
-
-# function created using "02 - Churned Customer Study" notebook code - Parallel Plot section
-def parallel_plot_churn(df_eda):
-
-    # hard coded from "disc.binner_dict_['tenure']"" result,
-    tenure_map = [-np.Inf, 6, 12, 18, 24, np.Inf]
-    # found at "02 - Churned Customer Study" notebook
-    # under "Parallel Plot" section
-    disc = ArbitraryDiscretiser(binning_dict={'tenure': tenure_map})
-    df_parallel = disc.fit_transform(df_eda)
-
-    n_classes = len(tenure_map) - 1
-    classes_ranges = disc.binner_dict_['tenure'][1:-1]
-    LabelsMap = {}
-    for n in range(0, n_classes):
-        if n == 0:
-            LabelsMap[n] = f"<{classes_ranges[0]}"
-        elif n == n_classes-1:
-            LabelsMap[n] = f"+{classes_ranges[-1]}"
-        else:
-            LabelsMap[n] = f"{classes_ranges[n-1]} to {classes_ranges[n]}"
-
-    df_parallel['tenure'] = df_parallel['tenure'].replace(LabelsMap)
-    fig = px.parallel_categories(
-        df_parallel, color="Churn", width=750, height=500)
-    # we use st.plotly_chart() to render, in notebook is fig.show()
-    st.plotly_chart(fig)
