c651e44f0f272e682f86d8b9ba9f2e840b76ed15
Agnieszka Lukasiak <lukasiak@me.com>
2025-01-22 16:20:45 +0000

    clean the data collection notebook, fix markdown, remove unecessary code

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
 
-
-
 # Load page scripts
 from app_pages.hypothesis_validation import page_hypothesis_validation_body
 from app_pages.predictor_pipeline import page_pipeline_overview
 from app_pages.profit_predictor import page_predictor_body
 from app_pages.project_summary import page_project_summary_body
 from app_pages.film_success_study import page_film_success_study_body
-
-
-
 
 # Create an instance of the MultiPage app
 app = MultiPage(app_name="Movie Revenue Predictor 🎬")
@@ -22,9 +17,6 @@ app.add_page("Study", page_film_success_study_body)
 app.add_page("Profit Predictor", page_predictor_body)
 app.add_page("Hypothesis Validation", page_hypothesis_validation_body)
 app.add_page("Predictor Pipeline", page_pipeline_overview)
-
-
-
 
 # Run the app
 app.run()
